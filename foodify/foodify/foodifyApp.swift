ebaca4dc9d57ac69aba3458ab3263866a028b4ad
Bora <borabalos9@gmail.com>
2024-06-23 17:13:29 +0200

    Project creation

---
+++ foodify/foodify/foodifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodifyApp.swift
+//  foodify
+//
+//  Created by Bora Balos on 23/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct foodifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

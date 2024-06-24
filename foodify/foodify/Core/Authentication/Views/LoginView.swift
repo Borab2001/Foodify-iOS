9583d90b2836cdc30f4f5294f3f8d9295dbeab42
Bora <borabalos9@gmail.com>
2024-06-24 15:13:52 +0200

    Cleaning for login view

--- foodify/foodify/Core/Authentication/Views/LoginView.swift
+++ foodify/foodify/Core/Authentication/Views/LoginView.swift
@@ -13,76 +13,64 @@ struct LoginView: View {
     
     var body: some View {
         NavigationStack {
-            ZStack {
-                Image("home-bg") //
-                    .resizable()
-                    .scaledToFill()
-                    .ignoresSafeArea()
+            VStack {
+                Spacer()
                 
-                LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.8), Color.black.opacity(0.6), Color.black.opacity(0.25), Color.clear]), startPoint: .bottom, endPoint: .top)
-                    .ignoresSafeArea()
+                Image("foodify-app-icon-white")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 120)
+                    .padding()
                 
                 VStack {
-                    Spacer()
+                    TextField("Enter your email", text: $email)
                     
-                    Image("foodify-app-icon-white")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 120, height: 120)
-                        .padding()
-                    VStack {
-                        TextField("Enter your email", text: $email)
-                        
-                        SecureField("Enter your password", text: $password)
-                    }
-                    
-                    NavigationLink {
-                        Text("Forgot password")
-                    } label: {
-                        Text("Forgot Password?")
-                            .font(.footnote)
-                            .fontWeight(.semibold)
-                            .padding(.top)
-                            .padding(.trailing, 28)
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity, alignment: .trailing)
-                    }
-                    
-                    Button {
-                        
-                    } label: {
-                        Text("Login")
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .foregroundColor(.black)
-                            .frame(width: 352, height: 44)
-                            .background(.white)
-                            .cornerRadius(8)
-                    }
-                    
-                    Spacer()
-                    
-                    Divider()
-                    
-                    NavigationLink {
-                        Text("Registration View")
-                    } label: {
-                        HStack(spacing: 3) {
-                            Text("Don't have an account?")
-                            
-                            Text("Sign Up")
-                                .fontWeight(.semibold)
-                        }
-                        .font(.footnote)
-                        .foregroundColor(.white)
-                        .padding()
-                    }
-                    .padding(.vertical, 16)
+                    SecureField("Enter your password", text: $password)
                 }
-                .padding(.top) // Ensure content does not overlap with safe area
-                .padding([.leading, .trailing]) // Ensure content does not overlap with safe area
-                .padding(.bottom)
+                
+                NavigationLink {
+                    Text("Forgot password")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.black)
+                        .frame(width: 352, height: 44)
+                        .background(.white)
+                        .cornerRadius(8)
+                }
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Registration View")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                    .foregroundColor(.white)
+                }
+                .padding(.vertical, 16)
             }
+            .background(.black)
         }
     }
 }

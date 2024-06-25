7d62878517e460e16139b840326e48c27f8a8a2d
Bora <borabalos9@gmail.com>
2024-06-25 15:21:42 +0200

    Placeholder modifier extension + placeholder color in registration view

---
+++ foodify/foodify/Extensions/PlaceholderModifier.swift
@@ -0,0 +1,34 @@
+//
+//  PlaceholderModifier.swift
+//  foodify
+//
+//  Created by Bora Balos on 25/06/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct PlaceholderModifier: ViewModifier {
+    var placeholder: String
+    var showPlaceholder: Bool
+    var placeholderColor: Color
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .leading) {
+            if showPlaceholder {
+                Text(placeholder)
+                    .foregroundColor(placeholderColor)
+                    .padding(.horizontal, 12)
+            }
+            content
+                .foregroundColor(.primary)
+                .padding(.horizontal, 12)
+        }
+    }
+}
+
+extension View {
+    func placeholder(_ placeholder: String, show: Bool, color: Color) -> some View {
+        self.modifier(PlaceholderModifier(placeholder: placeholder, showPlaceholder: show, placeholderColor: color))
+    }
+}

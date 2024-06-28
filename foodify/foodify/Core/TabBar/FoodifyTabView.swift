f6d8389b01a49bb716583dd65f8a4db231602041
Bora <borabalos9@gmail.com>
2024-06-28 15:24:09 +0200

    All tab buttons with filling and coloring

--- foodify/foodify/Core/TabBar/FoodifyTabView.swift
+++ foodify/foodify/Core/TabBar/FoodifyTabView.swift
@@ -12,12 +12,49 @@ struct FoodifyTabView: View {
     
     var body: some View {
         TabView(selection: $selectedTab) {
-            Text("Feed View")
+            Text("Feed")
                 .tabItem { 
-                    Image(systemName: selectedTab == 0 ? "house.fill" : "house")
+                    Image(systemName: selectedTab == 0 ? "house" : "house")
                         .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
                 }
+                .onAppear { selectedTab = 0 }
+                .tag(0)
+            
+            Text("Explore")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                        
+                }
+                .onAppear { selectedTab = 1 }
+                .tag(1)
+            
+            Text("Fridge")
+                .tabItem {
+                    Image(systemName: selectedTab == 2 ? "refrigerator" : "refrigerator")
+                        .environment(\.symbolVariants, selectedTab == 2 ? .fill : .none)
+                        
+                }
+                .onAppear { selectedTab = 2 }
+                .tag(2)
+            
+            Text("Shopping List")
+                .tabItem {
+                    Image(systemName: selectedTab == 3 ? "bag.fill" : "bag")
+                        .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                        
+                }
+                .onAppear { selectedTab = 3 }
+                .tag(3)
+            
+            Text("Calendar")
+                .tabItem {
+                    Image(systemName: "calendar")
+                        
+                }
+                .onAppear { selectedTab = 4 }
+                .tag(4)
         }
+        .tint(.black)
     }
 }
 

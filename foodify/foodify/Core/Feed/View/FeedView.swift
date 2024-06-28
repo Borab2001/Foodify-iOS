7f851ba6c7d3005dd78f4d22525586ab082d5736
Bora <borabalos9@gmail.com>
2024-06-28 16:50:55 +0200

    Boolean logic between sidebar and feed views

--- foodify/foodify/Core/Feed/View/FeedView.swift
+++ foodify/foodify/Core/Feed/View/FeedView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct FeedView: View {
+    @State var showSidebar: Bool = false
+    
     var body: some View {
         Text("Feed View")
+        
+        NavigationView {
+            HStack(spacing: 0) {
+                FoodifySidebarView(showSidebar: $showSidebar)
+            }
+        }
     }
 }
 
---
+++ foodify/foodify/Core/Sidebar/FoodifySidebarView.swift
@@ -0,0 +1,20 @@
+//
+//  FoodifySidebarView.swift
+//  foodify
+//
+//  Created by Bora Balos on 28/06/2024.
+//
+
+import SwiftUI
+
+struct FoodifySidebarView: View {
+    @Binding var showSidebar: Bool
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
